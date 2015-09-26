7175234a50aebb4e6928766dd57103b08edbd367
Matt Edwards <mttdwrds@gmail.com>
2015-09-26 17:51:39 -0400

    Deleted scripts that are redundant with the docs now.

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo docker run -it --rm --privileged \
-    --device /dev/nvidiactl \
-    --device /dev/nvidia-uvm \
-    --device /dev/nvidia0 \
-    --device /dev/nvidia1 \
-    --device /dev/nvidia2 \
-    matted/deepbind /bin/bash
